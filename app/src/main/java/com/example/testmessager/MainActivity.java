095ba2ca5761232da372b2d004ce88864f7c0cc6
ig <ig@pss.one>
2019-07-12 12:29:46 +0300

    Add AuthUI Add login,logout buttons

--- app/src/main/java/com/example/testmessager/MainActivity.java
+++ app/src/main/java/com/example/testmessager/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,10 +20,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Messager
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("messages");
     Button send;
@@ -30,28 +42,72 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     String message;
     private final int MESSAGE_MAX_LENTH=100;
-
+    //Messager
+    //Auth
+    private static final int MY_REQUEST_CODE = 7117;
+    List<AuthUI.IdpConfig> providers;
+    Button logout;
+    Button login;
+    //Auth
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Auth
+        logout =findViewById(R.id.buttonLogout);
+        login =findViewById(R.id.buttonLogin);
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
 
+        );
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //logout
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                logout.setEnabled(false);
+                                showSignInOptions();
+                                Toast.makeText(MainActivity.this,"Logout",Toast.LENGTH_SHORT);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this," "+e.getMessage(),Toast.LENGTH_SHORT);
+
+                    }
+                });
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignInOptions();
+            }
+        });
+        //Auth
+        //Messager
         myRef.setValue("Message");
         final ArrayList<String> messages = new ArrayList<>();
-
-
         send = findViewById(R.id.buttonSend);
         editText =findViewById(R.id.editText);
         recyclerView =findViewById(R.id.rectclerView);
-
         recyclerView.setLayoutManager( new LinearLayoutManager(this));
         final DataAdapter dataAdapter = new DataAdapter(this,messages);
-
         recyclerView.setAdapter(dataAdapter);
+        //Messager
 
 
+
+
+        //Messager
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
@@ -102,8 +158,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //Messager
+
 
 
 
     }
+    //Auth
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                        .build(), MY_REQUEST_CODE
+        );
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (requestCode == RESULT_OK){
+                //Get user
+                FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+                //Show email toast
+                Toast.makeText(this," "+user.getEmail(),Toast.LENGTH_SHORT);
+                logout.setEnabled(true);
+            }else {
+                Toast.makeText(this," "+response.getError(),Toast.LENGTH_SHORT);
+            }
+        }
+
+    }
+    //Auth
+
 }
